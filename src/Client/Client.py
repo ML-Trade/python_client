36a86910447a01bd238ddf5b92084601851d59bd
Kyle Doidge <kyle.blue.nuttall@gmail.com>
2021-10-10 18:23:57 +0100

    Working subscriber connection

--- src/Client/Client.py
+++ src/Client/Client.py
@@ -5,22 +5,17 @@ from Connection import Connection, ConnectionType
 class Client:
     def __init__(self):
         self.loop = False
-        # self.connection = Connection(ConnectionType.SUB, Globals.PUBLISHER_PORT)
+        self.subscriber = Connection(ConnectionType.SUB, Globals.PUBLISHER_PORT)
         print(f"Connected to publisher on port {Globals.PUBLISHER_PORT}")
 
     def start(self):
         self.loop = True
-        sub = Globals.context.socket(zmq.SUB)
-        sub.connect("tcp://0.0.0.0:25000")
-        sub.subscribe("")
-        print("CONNECTION BITHC")
         while self.loop:
-            ret = sub.recv_string()
-            print(ret)
             # Now, we wait
-            # result = self.connection.recieve(no_wait=False)
-            # if result is None:
-            #     continue
-            # print(f"Putting {result} into model")
+            result = self.subscriber.recieve(no_wait=False)
+            if result is None:
+                print("Recieved invalid or no data")
+                continue
+            print(f"Putting {result} into model")
             # Get prediction
             # Send recommendation back to MQL
--- src/Connection/Connection.py
+++ src/Connection/Connection.py
@@ -1,6 +1,8 @@
 from typing import Dict, List, Union
 import zmq
 from enum import Enum
+
+from zmq.sugar.context import T
 from Client.Globals import context
 import json
 
@@ -25,6 +27,9 @@ JSONDict = Dict[str, JSONValue]
 class Connection:
     def __init__(self, connectionType: ConnectionType, port: int) -> None:
         self.socket = context.socket(connectionType.value)
+        """ # TODO: NOTE If this is run in WSL, you must connect to the IP shown from:
+        grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'
+        See https://superuser.com/questions/1535269/how-to-connect-wsl-to-a-windows-localhost """
         self.socket.connect(f"tcp://localhost:{port}")
         self.socket.subscribe('')
         self.port = port
@@ -37,8 +42,11 @@ class Connection:
             message = self.socket.recv_string(flags=zmq.NOBLOCK)
         else:
             message = self.socket.recv_string()
-        return json.loads(message)
-
+        try:
+            return json.loads(message)
+        except json.JSONDecodeError:
+            # This is not JSON
+            return None
 
     def recieveAll(self) -> List[JSONDict]:
         """Gets messages using NO_BLOCK flag until no more can be received.
